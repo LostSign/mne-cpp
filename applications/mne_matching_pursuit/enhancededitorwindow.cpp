5e19c66b6cf45c39733f001ae76dabdb755a0795
GBeret <g-beret@gmx.de>
2016-02-28 18:24:42 +0100

    chg: namespaces

--- applications/mne_matching_pursuit/enhancededitorwindow.cpp
+++ applications/mne_matching_pursuit/enhancededitorwindow.cpp
@@ -85,7 +85,7 @@ QStringList _names_list;
 Enhancededitorwindow::Enhancededitorwindow(QWidget *parent): QWidget(parent), ui(new Ui::Enhancededitorwindow)
 {
     this->setAccessibleName("formel");
-    Ui->setupUi(this);
+    ui->setupUi(this);
 
     QSettings settings;
     move(settings.value("pos_enhanced_editor", QPoint(200, 200)).toPoint());
